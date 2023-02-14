e01592ca93a1ab2c221e2eaae7b1f6ffb204a733
Roemer <r.baeriswyl@gmail.com>
2023-02-14 09:23:54 +0100

    Improved run message, Fixed reopening of terminal if it was closed

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,20 @@
 import * as vscode from "vscode";
 import GotaskrCodeLensProvider from "./codelens";
 import { installGotaskrRunTaskCommand } from "./gotaskrRunTaskCommand";
+import TerminalRunner from "./terminalRunner";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Loaded extension "gotaskr-vscode"!');
 
+  if (("onDidCloseTerminal" in vscode.window) as any) {
+    (vscode.window as any).onDidCloseTerminal((terminal: vscode.Terminal) => {
+      TerminalRunner.onDidCloseTerminal(terminal);
+    });
+  }
+
   context.subscriptions.push(
     vscode.commands.registerCommand("gotaskr-vscode.runTask", async (taskName: string, fileName: string) => {
-      vscode.window.showInformationMessage("runTask from gotaskr-vscode");
+      vscode.window.showInformationMessage(`Running gotaskr task "${taskName}"`);
 
       installGotaskrRunTaskCommand(taskName, fileName, context);
     })
